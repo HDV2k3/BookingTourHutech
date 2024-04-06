ab6114ff9ae5bb23667b87cdb5388f4466d82980
Việt Huỳnh <dacviethuynh@gmail.com>
2024-04-06 15:35:09 +0700

    update database

---
+++ BookingTourHutech/Migrations/20240406083455_BookingTour12.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingTourHutech.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookingTour12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PersonCount",
+                table: "DetailBookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersonCount",
+                table: "DetailBookings");
+        }
+    }
+}
